e1ce7e8bf78922ac368fb0d4ddb41b85044487f9
johnlombardi389 <lombardij38@gmail.com>
2022-12-19 19:53:16 -0500

    Set up camera and added to scene

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cubeMesh);
+
+// Camera
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  1,
+  100
+);
+scene.add(camera);
