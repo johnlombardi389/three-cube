0a0138c8f591d91b103bc2537fc9ee61a6ecc584
johnlombardi389 <lombardij38@gmail.com>
2022-12-20 10:48:35 -0500

    Handled pixel ratio by setting ratio to not go above 2

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Animate
 
