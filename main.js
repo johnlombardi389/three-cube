fbe08f3596ed2916c71002951909b5086e97b8a9
johnlombardi389 <lombardij38@gmail.com>
2022-12-20 00:21:30 -0500

    Added event listener to check for resize and make adjustments to size of render

--- main.js
+++ main.js
@@ -19,6 +19,17 @@ const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+});
+
 const camera = new THREE.PerspectiveCamera(
   45,
   sizes.width / sizes.height,
