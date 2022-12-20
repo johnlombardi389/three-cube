2745d5c900cb22d7e2befffdb7be0ad83b5f041e
johnlombardi389 <lombardij38@gmail.com>
2022-12-19 19:51:17 -0500

    Imported Three.js and set up scene with cube object

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import * as THREE from "three";
+
+// Scene
+const scene = new THREE.Scene();
+
+// Cube
+const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
